4c169123147d750b0767fdb52bd1d101dde86472
Shadat Rahman <shadat.rahman.464@gmail.com>
2022-10-04 07:26:13 +0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shadat Rahman on 4/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
